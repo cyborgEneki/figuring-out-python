2f1eb0a8084aba235c1bd76e07aaa2fd3c2356bc
Eneki <joanreneki@gmail.com>
2022-03-03 15:26:55 +0300

    Modify letter f algorithm

--- letter-f.py
+++ letter-f.py
@@ -2,8 +2,8 @@ numbers = [5, 2, 5, 2, 2]
 length_of_outer_loop = len(numbers)
 string = ''
 
-for i in range(len(numbers)):
-    for j in range(numbers[i]):
+for i in numbers:
+    string = ''
+    for j in range(i):
         string += 'x'
     print(string)
-    string = ''
